2cab2e2372746d728134ff090fe877ec85933c3f
Fadeev Ivan <ivanfadeev2003@mail.ru>
2023-10-11 15:35:47 +0300

    fixes

--- src/main/java/com/example/testapplication/exception/massage/RoleExceptionMassage.java
+++
@@ -1,5 +0,0 @@
-package com.example.testapplication.exception.massage;
-
-public class RoleExceptionMassage {
-  public static final String ROLE_WITH_NAME_IS_EXIST = "Role with name %s is exist.";
-}
